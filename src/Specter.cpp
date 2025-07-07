6f0ea243cb66a7123916d38be32acb6ae6057212
LionX <aykhanmelikov67@gmail.com>
2025-07-07 18:39:00 +0400

    update gitignore and main files

--- src/Specter.cpp
+++ src/Specter.cpp
@@ -1,5 +1,4 @@
-﻿// Specter.cpp : Defines the entry point for the application.
-//
+﻿// main file
 
 #include "Specter.h"
 #include <memory.hpp>
@@ -17,22 +16,4 @@ int main()
     logger.Log(L"Press any key to exit the Specter");
     system("pause > nil");
     return 0;
-}
-
-void LoggerInit() {
-    Logger& logger = Logger::GetInstance();
-    logger.SetLogFile("logs.txt");
-    logger.SetMinLogLevel(LogLevel::DEBUG);
-    logger.SetLogFormat("{timestamp} | {level} | {source} | {message}");
-
-    logger.Log(LogLevel::INFO, L"logger.cpp", L"Logger successfully initialized!");
-    std::this_thread::sleep_for(std::chrono::milliseconds(600));
-
-    logger.DeleteLogFile("logs.txt");
-    logger.Log(LogLevel::INFO, L"logger.cpp", 32, L"Log file deleted -> -logs.txt");
-    std::this_thread::sleep_for(std::chrono::milliseconds(1200));
-
-    logger.CreateLogFile("logs.txt");
-    std::this_thread::sleep_for(std::chrono::milliseconds(60));
-    logger.Log(LogLevel::INFO, L"logger.cpp", 36, L"Log file created successfully -> +logs.txt");
 }
--- src/headers/Specter.h
+++ src/headers/Specter.h
@@ -1,9 +1,20 @@
-﻿// Specter.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
+﻿#pragma once
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
-void LoggerInit();
+void LoggerInit() {
+    Logger& logger = Logger::GetInstance();
+    logger.SetLogFile("logs.txt");
+    logger.SetMinLogLevel(LogLevel::DEBUG);
+    logger.SetLogFormat("{timestamp} | {level} | {source} | {message}");
+
+    logger.Log(LogLevel::INFO, L"logger.cpp", L"Logger successfully initialized!");
+    std::this_thread::sleep_for(std::chrono::milliseconds(600));
+
+    logger.DeleteLogFile("logs.txt");
+    logger.Log(LogLevel::INFO, L"logger.cpp", 32, L"Log file deleted -> -logs.txt");
+    std::this_thread::sleep_for(std::chrono::milliseconds(1200));
+
+    logger.CreateLogFile("logs.txt");
+    std::this_thread::sleep_for(std::chrono::milliseconds(60));
+    logger.Log(LogLevel::INFO, L"logger.cpp", 36, L"Log file created successfully -> +logs.txt");
+}
