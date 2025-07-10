44e48a5d5eb6fd04850ba780a3d46a6a1134befa
Kha0sK1ng <aykhanmelikov67@gmail.com>
2025-07-10 21:39:21 +0400

    Added some game offsets

--- include/utils/offsets.hpp
+++ include/utils/offsets.hpp
@@ -12,6 +12,10 @@ namespace Offsets {
     constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x18580D0;
     constexpr std::ptrdiff_t dwLocalPlayerController = 0x1A52D20;
     constexpr std::ptrdiff_t dwEntityList = 0x1A044E0;
+    constexpr std::ptrdiff_t m_iHealth = 0x344; // int32
+    constexpr std::ptrdiff_t m_iMaxHealth = 0x340; // int32
+    constexpr std::ptrdiff_t m_iTeamNum = 0x3E3; // uint8
+    constexpr std::ptrdiff_t m_fFlags = 0x3EC; // uint32
 
     bool UpdateOffsets(ProcessHelper& memory);
 }
