1d20d4418099d7daf192c450d64b086025c1d9c4
Kha0sK1ng <aykhanmelikov67@gmail.com>
2025-07-10 21:10:06 +0400

    added some offsets

--- include/utils/offsets.hpp
+++ include/utils/offsets.hpp
@@ -9,5 +9,9 @@
 namespace Offsets {
     inline uintptr_t client_dll = 0;
 
+    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x18580D0;
+    constexpr std::ptrdiff_t dwLocalPlayerController = 0x1A52D20;
+    constexpr std::ptrdiff_t dwEntityList = 0x1A044E0;
+
     bool UpdateOffsets(ProcessHelper& memory);
 }
