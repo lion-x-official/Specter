e86f1ff6f1e0dffa5b5df3b39737b61b83b0305f
Kha0sK1ng <aykhanmelikov67@gmail.com>
2025-07-10 21:42:23 +0400

    Added vars in game.hpp

--- include/game.hpp
+++ include/game.hpp
@@ -7,6 +7,14 @@
 
 class Game{
 public:
+	namespace Vars {
+		namespace LocalPlayerPawn {
+			inline int32_t health{ 0 };
+			inline int32_t maxHealth{ 0 };
+			inline uint8_t teamNum{ 0 };
+			inline uint32_t flags{ 0 };
+		}
+	}
 	static bool Initialize(ProcessHelper& memory);
 	static DWORD64 GetLocalPlayerPawn(ProcessHelper& memory);
 };
