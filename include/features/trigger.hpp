a095cbbfe889fc68f576fe26d2a81f605b5149ed
Kha0sK1ng <aykhanmelikov67@gmail.com>
2025-07-16 19:56:50 +0400

    refactor trigger state

---
+++ include/features/trigger.hpp
@@ -0,0 +1,19 @@
+<<<<<<< Updated upstream
+=======
+#pragma once
+#include "core/ProcessHelper.hpp"
+#include "utils/globals.hpp"
+
+class Trigger {
+public:
+	bool Update(ProcessHelper& memory);
+
+	void setEnabled(bool value) {
+		Globals::triggerEnabled = value;
+	}
+
+	bool getStatus() const {
+		return Globals::triggerEnabled;
+	}
+};
+>>>>>>> Stashed changes
---
+++ src/features/trigger.cpp
@@ -0,0 +1,11 @@
+<<<<<<< Updated upstream
+=======
+#include "features/trigger.hpp"
+
+bool Trigger::Update(ProcessHelper& memory) {
+	if (Globals::triggerEnabled) {
+
+	}
+	return true;
+}
+>>>>>>> Stashed changes
